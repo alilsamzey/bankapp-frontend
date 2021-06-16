53f5af3f839652f3b9e12e5388aad8549eefc6e2
alilsamzey <alilsamzey@gmail.com>
2021-06-16 16:29:51 +0200

    f

--- src/banner/Banner.js
+++ src/banner/Banner.js
@@ -12,7 +12,8 @@ function Banner() {
         <input type="radio" name="fancy" value="hearts" id="hearts" />
         <input type="radio" name="fancy" value="spades" id="spades" />
        			
-        
+        <label for="clubs">&#9827; Clubs</label><label for="hearts">&#9829; Hearts</label><label for="spades">&#9824; Spades</label><label for="diamonds">&#9830; Diamonds</label>
+
 
         <div className="keys">Use left and right keys to navigate</div>
 </form>
