373239516fbd273976fb67376ca4b693d0d440a8
alilsamzey <alilsamzey@gmail.com>
2021-06-16 20:32:51 +0200

    gh

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,43 +1,29 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Form, FormControl, Button   } from 'react-bootstrap';
 
-import {Link} from "react-router-dom"
-import logo from "../images/logo.png"
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-export default function ButtonAppBar() {
-  const classes = useStyles();
-
+function Header() {
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar variant= 'dense'>
-          <Link to="/">
-            <img src={logo} alt= "logo" />
-          </Link>
-          <Typography variant="h6" className={classes.title}>
-            CIFTLIK BANK
-          </Typography>
-          <Button color="inherit" href = ''>ABOUT</Button>
-          <Button color="inherit" href = ''>PRODUCT</Button>
-          <Button color="inherit" href = ''>PACKAGE</Button>
-          <Button color="inherit" href = ''>CONTACT</Button>
-        </Toolbar>
-      </AppBar>
+    <div>
+    
+  <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-light">Search</Button>
+    </Form>
+  </Navbar>
+
+ 
+
+      
     </div>
-  );
-}
+  )
+};
+
+export default Header
